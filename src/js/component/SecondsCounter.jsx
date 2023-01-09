41c64c5d0fc317451845132bbc6aa399cf35c4f7
sumpierrezf <viqui09umpierrez@gmail.com>
2023-01-09 23:19:37 +0000

    Finalizando parte estetica

--- src/js/component/SecondsCounter.jsx
+++ src/js/component/SecondsCounter.jsx
@@ -7,13 +7,30 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const SecondsCounter = (props) => {
 	return (
 		<>
-		<div className="d-flex">
-		<p>Contador: <i class="fa-solid fa-clock"></i> {props.numero6}</p>
-		<p>{props.numero5}</p>
-		<p>{props.numero4}</p>
-		<p>{props.numero3}</p>
-		<p>{props.numero2}</p>
-		<p>{props.numero1}</p>
+		<div className="container text-center d-flex bg-black m-auto justify-content-center">
+			<div className="row">
+				<div className="col fs-1 bg-white mt-2 mb-2">
+				<i className="fa fa-clock"></i>
+				</div>
+				<div className="col">
+				<p className="fs-1 text-white">{props.numero6}</p>
+				</div>
+				<div className="col">
+				<p className="fs-1 text-white">{props.numero5}</p>
+				</div>
+				<div className="col">
+				<p className="fs-1 text-white">{props.numero4}</p>
+				</div>
+				<div className="col">
+				<p className="fs-1 text-white">{props.numero3}</p>
+				</div>
+				<div className="col">
+				<p className="fs-1 text-white">{props.numero2}</p>
+				</div>
+				<div className="col">
+				<p className="fs-1 text-white">{props.numero1}</p>
+				</div>
+			</div>
 		</div>
 		</>
 	)
